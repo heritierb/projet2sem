e01d512f1494e1d49cafd20b4a4603eaead02ebd
ben <ben@DESKTOP-LHFKREL>
2018-05-05 12:14:50 +0200

    test

---
+++ Eclipse/Projet_2ème semestre/src/Projet_2sem/Contact.java
@@ -0,0 +1,27 @@
+package Projet_2sem;
+
+public class Contact {
+
+	String Nom;
+	String Prenom;
+	int numero;
+	String email;
+	String adresse;
+	int anniversaire;
+	String groupe;
+	
+	public Contact(String nom, String prenom, int numero, String email,
+			String adresse, int anniversaire, String groupe) {
+		
+		this.Nom = nom;
+		this.Prenom = prenom;
+		this.numero = numero;
+		this.email = email;
+		this.adresse = adresse;
+		this.anniversaire = anniversaire;
+		this.groupe = groupe;
+		
+	}
+	
+	
+}
