61aebe2bc34664f5b9ec95848d429419de76622f
ben <ben@DESKTOP-LHFKREL>
2018-05-08 19:54:09 +0200

    add class AfficheImage

---
+++ NetBeans/Projet_2ème semestre/src/graphisme/AfficheImage.java
@@ -0,0 +1,32 @@
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Graphics;
+
+public class AfficheImage {
+
+	public static void main(String[] args) {
+
+		JFrame fen = new JFrame("Ordinateur ");
+		JPanel pan = new JPanel();
+		ImageIcon icon = new ImageIcon(
+				"C:/Users/oppik/Pictures/Screenshots/Capture.jpg");
+		JLabel img = new JLabel(icon);
+		pan.add(img);
+		fen.add(pan);
+		fen.pack();
+		fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fen.setVisible(true);
+
+	}
+}
