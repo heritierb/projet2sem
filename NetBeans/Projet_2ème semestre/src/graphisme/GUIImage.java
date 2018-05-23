dc829717efed9452d860f3e5e682b688e70c6aed
ben <ben@DESKTOP-LHFKREL>
2018-05-23 17:55:31 +0200

    GUIImage

--- NetBeans/Projet_2ème semestre/src/graphisme/GUIImage.java
+++ NetBeans/Projet_2ème semestre/src/graphisme/GUIImage.java
@@ -1,6 +1,21 @@
 package graphisme;
 
-public class GUIImage {
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class GUIImage extends JPanel {
+    
+    private ImagePanel image = new ImagePanel(new ImageIcon("src/images/wallpaper2.png"));
+    private GUITelephone guit = (GUITelephone) SwingUtilities.getAncestorOfClass(GUITelephone.class, GUIImage.this);
+    
+    public GUIImage(GUITelephone guit){
+        this.guit = guit;
+        add(image);
+        
+        
+    }
+    
 
 	
 }
