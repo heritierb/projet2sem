a7deb3ac9f7b95a9e6f564a92cfcd438f2516a62
oppik <oppik@10.91.129.162>
2018-05-23 14:36:25 +0200

    GuiGallerie adapté

--- NetBeans/Projet_2ème semestre/src/graphisme/GUIGallerie.java
+++ NetBeans/Projet_2ème semestre/src/graphisme/GUIGallerie.java
@@ -11,63 +11,50 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GUIGallerie extends JPanel {
-        private ImagePanel gallerie = new ImagePanel(new ImageIcon("src/images/wallpaper2.png"));
-        private GUITelephone guit;
-        
-        public GUIGallerie(GUITelephone guit){
-            this.guit = guit;
-            setLayout(new BorderLayout());
-            add(gallerie);          
-        }
-        
-        
+	private ImagePanel gallerie = new ImagePanel(new ImageIcon(
+			"src/images/wallpaper2.png"));
+	private GUITelephone guit;
+
+	int TAILLE_BOUTON = 100;
+	int nb_items;
+
+	static String DOSSIER = "C:/Users/Projet/Photo_Projet/photos/";
+	String EXTENSION = ".jpg";
+	ArrayList<String> fichiers;
+	list_directory photos = new list_directory(DOSSIER, EXTENSION);
+	JFrame fen = new JFrame("Gallerie");
+	JPanel panelSouth = new JPanel();
+	JButton home = new JButton("Home");
+
+	public GUIGallerie(GUITelephone guit) {
+		this.guit = guit;
+		setLayout(new BorderLayout());
+
+		fichiers = list_directory.listFileNames(DOSSIER, EXTENSION);
+		nb_items = fichiers.size();
+
+		for (int i = 0; i < nb_items; i++) {
+			JButton cpt = CreationBoutonImage((String) photos.getFichiers()
+					.get(i));
+			gallerie.add(cpt);
+		}
+
+		add(gallerie);
+
+	}
+
+	public static JButton CreationBoutonImage(String name) {
+		ImageIcon imageBouton;
+		Image imageBoutonRedim;
+		JButton bouton;
+
+		imageBouton = new ImageIcon(DOSSIER.concat(name));
+		imageBoutonRedim = imageBouton.getImage().getScaledInstance(100, 100,
+				java.awt.Image.SCALE_SMOOTH);
+		imageBouton = new ImageIcon(imageBoutonRedim);
+		bouton = new JButton(imageBouton);
+
+		return bouton;
+	}
 }
-//	int TAILLE_BOUTON = 100;
-//	int nb_items;
-//	
-//	static String DOSSIER = "C:/Users/Projet/Photo_Projet/photos/";
-//	String EXTENSION = ".jpg";
-//	ArrayList<String> fichiers;
-//	list_directory photos = new list_directory(DOSSIER, EXTENSION);
-//	JFrame fen = new JFrame("Gallerie");
-//	JPanel panelSouth = new JPanel();
-//	JButton home = new JButton("Home");
-//
-//	public GUIGallerie() {
-//		fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		JPanel fondecran = new ImagePanel();
-//		fondecran.setPreferredSize(new Dimension(240, 400));
-//		
-//		fichiers = list_directory.listFileNames(DOSSIER, EXTENSION);
-//		nb_items = fichiers.size();
-//
-//		for (int i = 0; i < nb_items; i++) {
-//			JButton cpt = CreationBoutonImage((String) photos.getFichiers()
-//					.get(i));
-//			fondecran.add(cpt);
-//		}
-//		panelSouth.add(home);
-//		
-//		add(panelSouth, BorderLayout.SOUTH);
-//		add(fondecran);
-//		setResizable(false);
-//		pack();
-//		setLocationRelativeTo(null);
-//		setVisible(true);
-//
-//	}
-//
-//	public static JButton CreationBoutonImage(String name) {
-//		ImageIcon imageBouton;
-//		Image imageBoutonRedim;
-//		JButton bouton;
-//
-//		imageBouton = new ImageIcon(DOSSIER.concat(name));
-//		imageBoutonRedim = imageBouton.getImage().getScaledInstance(100, 100,
-//				java.awt.Image.SCALE_SMOOTH);
-//		imageBouton = new ImageIcon(imageBoutonRedim);
-//		bouton = new JButton(imageBouton);
-//
-//		return bouton;
-//	}
-//}
+
