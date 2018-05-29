59ecb51a4b7104f562713e72dfe170ea6c39d153
heritierb <37473089+heritierb@users.noreply.github.com>
2018-05-29 09:28:40 +0000

    Update+comments

--- NetBeans/Projet_2ème semestre/src/structure/Contact.java
+++ NetBeans/Projet_2ème semestre/src/structure/Contact.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Contact implements Serializable {
-
+    
+    // **** COMPOSANTS D'UN CONTACT **** //
     String prenom;
     String nom;
     String numero;
@@ -13,7 +14,8 @@ public class Contact implements Serializable {
     String adresse;
     String anniversaire;
     String groupe;
-
+    
+    // **** CONSTRUCTEUR **** //
     public Contact(String prenom, String nom, String numero, String email,
             String adresse, String anniversaire, String groupe) {
         this.prenom = prenom;
@@ -24,61 +26,47 @@ public class Contact implements Serializable {
         this.anniversaire = anniversaire;
         this.groupe = groupe;
     }
-
+    // **** GETTERS&SETTERS **** //
     public String getPrenom() {
         return prenom;
     }
-
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
-
     public String getNom() {
         return nom;
     }
-
     public void setNom(String nom) {
         this.nom = nom;
     }
-
     public String getNumero() {
         return numero;
     }
-
     public void setNumero(String numero) {
         this.numero = numero;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getAdresse() {
         return adresse;
     }
-
     public void setAdresse(String adresse) {
         this.adresse = adresse;
     }
-
     public String getAnniversaire() {
         return anniversaire;
     }
-
     public void setAnniversaire(String anniversaire) {
         this.anniversaire = anniversaire;
     }
-
     public String getGroupe() {
         return groupe;
     }
-
     public void setGroupe(String groupe) {
         this.groupe = groupe;
     }
-
 }
