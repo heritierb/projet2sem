61ac0b1b9099ebf50374ab55e8bafea05d085fe5
hadoppi <37473060+hadoppi@users.noreply.github.com>
2018-06-05 17:32:46 +0200

    Update GUIImage.java
    affichage image simple

--- NetBeans/Projet_2ème semestre/src/graphisme/GUIImage.java
+++ NetBeans/Projet_2ème semestre/src/graphisme/GUIImage.java
@@ -1,30 +1,62 @@
 package graphisme;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
 public class GUIImage extends JPanel {
 
-	
-
 	private GUIGallerie guig = (GUIGallerie) SwingUtilities.getAncestorOfClass(
 			GUIGallerie.class, GUIImage.this);
+	private JPanel panelImage = new JPanel();
+	private JPanel panelImageEtScroll = new JPanel();
+	private JScrollPane panelScroll = new JScrollPane(panelImage);
 
 	public GUIImage(GUITelephone guit) {
 
-		
+	}
+	
+
+	
+
+	
+	public void update(){
+		this.removeAll();
 		ImageIcon icon = new ImageIcon(GUIGallerie.getImageAAfficher());
 		JLabel img = new JLabel(icon);
-		img.setPreferredSize(new Dimension(480, 683));
+		ImagePanel image = new ImagePanel(new ImageIcon("y"));
 		
-	
 		
-		System.out.println(GUIGallerie.getImageAAfficher());
+				
+		panelImage.setLayout(new FlowLayout());
+		panelImage.setOpaque(false);
+		panelImage.setBackground(null);
+		panelImage.setBorder(null);
+		
+		
+		panelImageEtScroll.setLayout(new BorderLayout());
+		panelImageEtScroll.setOpaque(false);
+		panelImageEtScroll.setBackground(null);
+		panelImageEtScroll.setBorder(null);
+		panelImageEtScroll.setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+		
+		panelScroll.getViewport().setOpaque(false);
+		panelScroll.setOpaque(false);
+		panelScroll.setPreferredSize(new Dimension(15, 600));
+		
+		image.add(img);
+		add(image);
+		
+		
+		
+		add(panelImageEtScroll, BorderLayout.CENTER);
+		
 	}
 
 }
