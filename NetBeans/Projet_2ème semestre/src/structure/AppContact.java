b7a3eca9c997844d0c8818e8375428bdb393c541
heritierb <37473089+heritierb@users.noreply.github.com>
2018-06-07 16:25:53 +0200

    Update

--- NetBeans/Projet_2ème semestre/src/structure/AppContact.java
+++ NetBeans/Projet_2ème semestre/src/structure/AppContact.java
@@ -2,15 +2,17 @@ package structure;
 // ****                        **** //
 // **** AUTEUR BENOIT HERITIER **** //
 // ****                        **** //
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class AppContact implements Serializable {
-    
+
     // **** DECLARATION LISTE DYNAMIQUE DE CONTACTS **** //
     public ArrayList<Contact> arrayContacts = new ArrayList<Contact>();
-    
+
     // **** AJOUTE UN CONTACT **** //
     public void addC(Contact contact) {
         arrayContacts.add(contact);
@@ -19,18 +21,22 @@ public class AppContact implements Serializable {
         //            System.out.println("Prenom : " + c.getPrenom()+ " || Nom : " + c.getNom());
         //        }
     }
+
     // **** SUPPRIME UN CONTACT **** //
     public void deleteC(Contact contact) {
         arrayContacts.remove(contact);
     }
+
     // **** EDITER LES INFORMATIONS D'UN CONTACT **** //
     public void editC(Contact contact) {
         arrayContacts.set(arrayContacts.indexOf(contact), contact);
     }
+
     // **** GETTERS&SETTERS **** //
     public ArrayList<Contact> getArrayContacts() {
         return arrayContacts;
     }
+
     public void setArrayContacts(ArrayList<Contact> arrayContacts) {
         this.arrayContacts = arrayContacts;
     }
