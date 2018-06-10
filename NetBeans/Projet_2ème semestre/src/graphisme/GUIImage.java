d4aed324245609ebf8747af23b941effa7f86a5c
hadoppi <37473060+hadoppi@users.noreply.github.com>
2018-06-10 21:17:35 +0200

    Update GUIImage.java
    guiImage fait selon principe de la poo

--- NetBeans/Projet_2ème semestre/src/graphisme/GUIImage.java
+++ NetBeans/Projet_2ème semestre/src/graphisme/GUIImage.java
@@ -9,13 +9,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+
 import structure.AppContact;
+import structure.AppGallerie;
 import structure.Contact;
 
 public class GUIImage extends JPanel {
@@ -31,6 +34,7 @@ public class GUIImage extends JPanel {
 			.getAncestorOfClass(GUITelephone.class, GUIImage.this);
 
 	private ImageIcon icon = new ImageIcon(GUIGallerie.getImageAAfficher());
+	private ImageIcon iconSupprimer = new ImageIcon("src/images/cancel.png");
 	private JLabel img = new JLabel(icon);
 	private JPanel panelImage = new JPanel();
 	private JPanel panelImageEtScroll = new JPanel();
@@ -41,38 +45,60 @@ public class GUIImage extends JPanel {
 	private ImagePanel imageX = new ImagePanel(new ImageIcon(
 			"src/images/wallpaper.png"));
 	private JButton boutonSupprimer = new JButton();
-	private ArrayList<Contact> arrayC;
 	private JTextField txt = new JTextField("test");
 	private int hauteurFinale = 600;
 	private int largeurFinale = 480;
 	private int ratio;
+	private int hauteurOriginale;
+	private int largeurOriginale;
 	GUIContacts guic;
 	AppContact contactA;
 	Contact contactZ;
+	AppGallerie gallerieA;
+	GUINewContact guinewcontact;
+	GUIEditContact guieditcontact;
+	private ArrayList<Contact> arrayC;
+	String test = " ";
+	private ImageIcon iconX;
 
-	public GUIImage(GUITelephone guit, GUIContacts guic, AppContact contactA) {
-		this.contactZ = contactZ;
+	public GUIImage(GUITelephone guit, GUIContacts guic, AppContact contactA,
+			GUINewContact guinewcontact, GUIEditContact guieditcontact, AppGallerie gallerieA) {
+		this.guieditcontact = guieditcontact;
+		this.guinewcontact = guinewcontact;
 		this.guit = guit;
 		this.guic = guic;
 		this.contactA = contactA;
+		this.gallerieA = gallerieA;
 		arrayC = contactA.getArrayContacts();
 
-		icon = new ImageIcon(GUIGallerie.getImageAAfficher());
-		JLabel img = new JLabel(icon);
+		//icon = new ImageIcon(GUIGallerie.getImageAAfficher());
+		JLabel img = new JLabel(iconX);
 
+		panelImage.setLayout(new BorderLayout());
+		panelImage.setOpaque(false);
+		panelImage.setBackground(null);
+		panelImage.setBorder(null);
+		panelImage.setPreferredSize(new Dimension(480, 600));
+
+		panelImageEtScroll.setLayout(new BorderLayout());
+		panelImageEtScroll.setOpaque(false);
+		panelImageEtScroll.setBackground(null);
+		panelImageEtScroll.setBorder(null);
+		panelImageEtScroll.setPreferredSize(new Dimension(480, 600));
+
+		panelImageEtScroll.add(img);
+		add(panelImageEtScroll, BorderLayout.CENTER);
 	}
 
 	public void update() {
-
 		panelImageEtScroll.removeAll();
 		panelSupprimer.removeAll();
 		updateUI();
+		iconX = new ImageIcon(gallerieA.getFichiers().get(guig.getNumeroImage()).getImageI().getImage());
+		img = new JLabel(iconX);
 
-		icon = new ImageIcon(GUIGallerie.getImageAAfficher());
-		JLabel img = new JLabel(icon);
-
-		int hauteurOriginale = icon.getIconHeight();
-		int largeurOriginale = icon.getIconWidth();
+		hauteurOriginale = iconX.getIconHeight();
+		largeurOriginale = iconX.getIconWidth();
 
 		if (hauteurOriginale > 600) {
 			hauteurFinale = 600;
@@ -88,47 +114,74 @@ public class GUIImage extends JPanel {
 		} else
 			largeurFinale = largeurOriginale;
 
-		imageRedim = icon.getImage().getScaledInstance(largeurFinale,
+		imageRedim = iconX.getImage().getScaledInstance(largeurFinale,
 				hauteurFinale, java.awt.Image.SCALE_SMOOTH);
-		panelImage.setLayout(new BorderLayout());
-		panelImage.setOpaque(false);
-		panelImage.setBackground(null);
-		panelImage.setBorder(null);
-		panelImage.setPreferredSize(new Dimension(480, 600));
-
-		ImageIcon iconSupprimer = new ImageIcon("src/images/cancel.png");
-		boutonSupprimer = new JButton(iconSupprimer);
-		boutonSupprimer.addActionListener(new Suppression());
-
-		panelSupprimer.setLayout(new FlowLayout());
-		panelSupprimer.setBackground(Color.black);
-		panelSupprimer.add(boutonSupprimer);
-
-		panelImageEtScroll.setLayout(new BorderLayout());
-		panelImageEtScroll.setOpaque(false);
-		panelImageEtScroll.setBackground(null);
-		panelImageEtScroll.setBorder(null);
-		panelImageEtScroll.setPreferredSize(new Dimension(480, 600));
-
 		img.setIcon(new ImageIcon(imageRedim));
 		panelImageEtScroll.add(img);
-		add(panelImageEtScroll, BorderLayout.CENTER);
 		panelImageEtScroll.add(panelSupprimer, BorderLayout.SOUTH);
 		this.validate();
 		this.repaint();
 
 	}
 
-	private class Suppression implements ActionListener {
+	public void panelSuppressionImage() {
+		updateUI();
+		boutonSupprimer = new JButton(iconSupprimer);
+		boutonSupprimer.addActionListener(new Suppression());
+		panelSupprimer.setLayout(new FlowLayout());
+		panelSupprimer.setBackground(Color.black);
+		panelSupprimer.add(boutonSupprimer);
+
+		panelImageEtScroll.add(panelSupprimer, BorderLayout.SOUTH);
+
+		this.validate();
+		this.repaint();
+
+	}
+
+	public class Suppression implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
-			
+
 			File aSupprimer = new File(GUIGallerie.getImageAAfficher());
 			aSupprimer.delete();
-			//refresh la guigallerie
-			//revenir page précédente
-			
+			gallerieA.fichiers.remove(guig.getNumeroImage());
+			guit.setCurrentPanel("gallerie");
+		}
+	}
 
+	public void panelAddContact() {
+		panelSouth.removeAll();
+		updateUI();
+		panelSouth.setLayout(new BorderLayout());
+		panelSouth.setOpaque(false);
+		buttonSetImCt.setContentAreaFilled(false);
+		buttonSetImCt.setPreferredSize(new Dimension(120, 120));
+		buttonSetImCt.setBorderPainted(false);
+		panelSouth.add(buttonSetImCt);
+		buttonSetImCt.addActionListener(new GUIImage.setImCtClick());
+	}
+
+	// ** CHANGE LA PHOTO EN APPELANT L'APP GALLERIE ** //
+
+	private class setImCtClick implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+
+			if (guic.switcheditadd == 3) {
+				guinewcontact.user = new ImageIcon(
+						GUIGallerie.getImageAAfficher());
+				guit.setCurrentPanel("newcontact");
+				guinewcontact.refreshPhoto();
+				guit.backPosition = 5;
+
+			} else if (guic.switcheditadd == 2) {
+				guieditcontact.icone = new ImageIcon(
+						GUIGallerie.getImageAAfficher());
+				guit.setCurrentPanel("editcontact");
+				guieditcontact.refreshPhoto();
+				guit.backPosition = 6;
+			}
 		}
 	}
 }
