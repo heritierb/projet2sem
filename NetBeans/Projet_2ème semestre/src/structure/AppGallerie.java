b8f5c408d38660be64cc526f7696d86d5cb7a270
hadoppi <37473060+hadoppi@users.noreply.github.com>
2018-06-13 00:03:19 +0200

    Update AppGallerie.java

--- NetBeans/Projet_2ème semestre/src/structure/AppGallerie.java
+++ NetBeans/Projet_2ème semestre/src/structure/AppGallerie.java
@@ -1,60 +1,32 @@
 package structure;
-
-
-
 import java.io.Serializable;
 import java.util.ArrayList;
-
 
 public class AppGallerie implements Serializable{
 
 	private String DOSSIER = "src/photos/";
 	private String EXTENSION = ".jpg";
 	
-	public ArrayList<List_directory> fichiers= new ArrayList<List_directory>();
-//	private List_directory photos = new List_directory(DOSSIER, EXTENSION);
-	
-	
-	
+	public ArrayList<Photo> fichiers= new ArrayList<Photo>();
+	// **** getter and setters**** //
 	public String getDOSSIER() {
 		return DOSSIER;
 	}
-//	public void setDOSSIER(String dOSSIER) {
-//		DOSSIER = dOSSIER;
-//	}
-//	public String getEXTENSION() {
-//		return EXTENSION;
-//	}
-////	public void setEXTENSION(String eXTENSION) {
-////		EXTENSION = eXTENSION;
-//	}
-//	public List_directory getPhotos() {
-//		return photos;
-//	}
-//	public void setPhotos(List_directory photos) {
-//		this.photos = photos;
-//	}
-	public ArrayList<List_directory> getFichiers() {
+	
+	public ArrayList<Photo> getFichiers() {
 		return fichiers;
 	}
-	public void setFichiers(ArrayList<List_directory> fichiers) {
+	public void setFichiers(ArrayList<Photo> fichiers) {
 		this.fichiers = fichiers;
 	}
-	
-//	public AppGallerie(){
-//		fichiers = list_directory.listFileNames(DOSSIER, EXTENSION);
-//	}
-    public void addI(List_directory ld) {
+	// **** ajout d'une photo **** //
+    public void addI(Photo ld) {
     	fichiers.add(ld);
     }
-
-    public void deleteI(List_directory ld) {
+    // **** suppression d'une photo **** //
+    public void deleteI(Photo ld) {
     	fichiers.remove(ld);
     }
 
-    public void afficheUnit() 
-    {
-    	
-    }
     
 }
